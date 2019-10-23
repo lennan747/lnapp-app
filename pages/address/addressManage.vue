6f3f9bdd3d1d0647f45ebe04f0bf2331a4d0fee9
lennan747 <lennan747@gmail.com>
2019-10-23 23:06:51 +0800

    省市区三级联动

--- pages/address/addressManage.vue
+++ pages/address/addressManage.vue
@@ -10,16 +10,17 @@
 		</view>
 		<view class="row b-b">
 			<text class="tit">地址</text>
-			<text @click="chooseLocation" class="input">
+			<!-- <text @click="chooseLocation" class="input">
 				{{addressData.addressName}}
 			</text>
-			<text class="yticon icon-shouhuodizhi"></text>
+			<text class="yticon icon-shouhuodizhi"></text> -->
+		    <lotus-address v-on:choseVal="choseValue" :lotusAddressData="lotusAddressData"></lotus-address>
 		</view>
-		<view class="row b-b"> 
+		<view class="row b-b">
 			<text class="tit">门牌号</text>
 			<input class="input" type="text" v-model="addressData.area" placeholder="楼号、门牌" placeholder-class="placeholder" />
 		</view>
-		
+
 		<view class="row default-row">
 			<text class="tit">设为默认</text>
 			<switch :checked="addressData.defaule" color="#fa436a" @change="switchChange" />
@@ -29,6 +30,8 @@
 </template>
 
 <script>
+	import addressd from "@/components/jm-address/jm-address.vue"
+	import lotusAddress from "../../components/Winglau14-lotusAddress/Winglau14-lotusAddress.vue";
 	export default {
 		data() {
 			return {
@@ -39,60 +42,92 @@
 					address: '',
 					area: '',
 					default: false
-				}
+				},
+				lotusAddressData: {
+					visible: true,
+					provinceName: '北京',
+					cityName: '北京市',
+					townName: '东城区',
+				},
+				region: ''
 			}
 		},
-		onLoad(option){
+		components: {
+			"lotus-address": lotusAddress
+		},
+		onLoad(option) {
 			let title = '新增收货地址';
-			if(option.type==='edit'){
+			if (option.type === 'edit') {
 				title = '编辑收货地址'
-				
+
 				this.addressData = JSON.parse(option.data)
 			}
 			this.manageType = option.type;
 			uni.setNavigationBarTitle({
 				title
 			})
+			this.openPicker()
 		},
 		methods: {
-			switchChange(e){
+			//打开picker
+			openPicker() {
+				this.lotusAddressData.visible = true;
+				this.lotusAddressData.provinceName = '广东省';
+				this.lotusAddressData.cityName = '广州市';
+				this.lotusAddressData.townName = '白云区';
+			},
+			//回传已选的省市区的值
+			choseValue(res) {
+				//res数据源包括已选省市区与省市区code
+				console.log(res);
+				
+				this.lotusAddressData.visible = res.visible; //visible为显示与关闭组件标识true显示false隐藏
+				//res.isChose = 1省市区已选 res.isChose = 0;未选
+				if (res.isChose) {
+					this.lotusAddressData.provinceName = res.province; //省
+					this.lotusAddressData.cityName = res.city; //市
+					this.lotusAddressData.townName = res.town; //区
+					this.region = `${res.province} ${res.city} ${res.town}`; //region为已选的省市区的值
+				}
+			},
+			switchChange(e) {
 				this.addressData.default = e.detail;
 			},
-			
+
 			//地图选择地址
-			chooseLocation(){
+			chooseLocation() {
 				uni.chooseLocation({
-					success: (data)=> {
+					success: (data) => {
 						this.addressData.addressName = data.name;
 						this.addressData.address = data.name;
 					}
 				})
 			},
-			
+
 			//提交
-			confirm(){
+			confirm() {
 				let data = this.addressData;
-				if(!data.name){
+				if (!data.name) {
 					this.$api.msg('请填写收货人姓名');
 					return;
 				}
-				if(!/(^1[3|4|5|7|8][0-9]{9}$)/.test(data.mobile)){
+				if (!/(^1[3|4|5|7|8][0-9]{9}$)/.test(data.mobile)) {
 					this.$api.msg('请输入正确的手机号码');
 					return;
 				}
-				if(!data.address){
+				if (!data.address) {
 					this.$api.msg('请在地图选择所在位置');
 					return;
 				}
-				if(!data.area){
+				if (!data.area) {
 					this.$api.msg('请填写门牌号信息');
 					return;
 				}
-				
+
 				//this.$api.prePage()获取上一页实例，可直接调用上页所有数据和方法，在App.vue定义
 				this.$api.prePage().refreshList(data, this.manageType);
 				this.$api.msg(`地址${this.manageType=='edit' ? '修改': '添加'}成功`);
-				setTimeout(()=>{
+				setTimeout(() => {
 					uni.navigateBack()
 				}, 800)
 			},
@@ -101,45 +136,51 @@
 </script>
 
 <style lang="scss">
-	page{
+	page {
 		background: $page-color-base;
 		padding-top: 16upx;
 	}
 
-	.row{
+	.row {
 		display: flex;
 		align-items: center;
 		position: relative;
-		padding:0 30upx;
+		padding: 0 30upx;
 		height: 110upx;
 		background: #fff;
-		
-		.tit{
+
+		.tit {
 			flex-shrink: 0;
 			width: 120upx;
 			font-size: 30upx;
 			color: $font-color-dark;
 		}
-		.input{
+
+		.input {
 			flex: 1;
 			font-size: 30upx;
 			color: $font-color-dark;
 		}
-		.icon-shouhuodizhi{
+
+		.icon-shouhuodizhi {
 			font-size: 36upx;
 			color: $font-color-light;
 		}
 	}
-	.default-row{
+
+	.default-row {
 		margin-top: 16upx;
-		.tit{
+
+		.tit {
 			flex: 1;
 		}
-		switch{
+
+		switch {
 			transform: translateX(16upx) scale(.9);
 		}
 	}
-	.add-btn{
+
+	.add-btn {
 		display: flex;
 		align-items: center;
 		justify-content: center;
